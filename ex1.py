0357c3d2d7eb92daa4af44fcb008d41060dcd4a9
JeetChan <258110941@qq.com>
2017-07-10 21:21:46 +0800

    first commit

---
+++ ex1.py
@@ -0,0 +1,11 @@
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+
+# Study Drills
+print("中文测试。")
+
