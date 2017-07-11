51c576eaf4b9da117841bd32602988b11f96ae4e
JeetChan <258110941@qq.com>
2017-07-11 14:49:06 +0800

    ex11

---
+++ ex11.py
@@ -0,0 +1,8 @@
+print("How old are you?"),
+age = input()
+print("How tall are you?"),
+height = input()
+print("How much do you weigh?"),
+weight = input()
+# print("So, you're %r old, %r tall and %r heavy.") % (age, height, weight)
+print(f"So, you're {age} old, {height} tall and {weight} heavy.") 
