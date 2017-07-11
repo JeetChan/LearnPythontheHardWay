2a3d71fa16f7868bcc3b89c71450f7247435aed3
JeetChan <258110941@qq.com>
2017-07-11 15:31:15 +0800

    Parameters, Unpacking, Variables

---
+++ ex13.py
@@ -0,0 +1,6 @@
+from sys import argv
+script, first, second, third = argv
+print(f"The script is called:{script}")
+print(f"Your first variable is:{first}")
+print(f"Your second variable is:{second}")
+print(f"Your third variable is:{third}")
